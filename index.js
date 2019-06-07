d0c5bd8e3215bcc70da770dad99efb7f6d394876
Tochukwu Ezeani <nigakool@gmail.com>
2019-06-07 02:13:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-function holidayCountdown(holiday, days){
-returns (`It's ${days} days until ${holiday}`);
-holidayCountdown('70', easter);
-=======
-function happyHolidayTo(thirdterm, name){
-  return (`Happy ${holiday}, ${name}!`)
+function twin(me, you){
+  return (`I am a ${days} days until ${holiday}`)
+  
 }
-happyHolidayTo('easter, Armstrong');
->>>>>>> 5fa1715935709fe3d30f3b21e621dbfc69e0aba3
+twin("70","easter");
