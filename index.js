a156b33f61c1d488e3025ab28ca7e315d6be2091
Tochukwu Ezeani <nigakool@gmail.com>
2019-06-07 01:54:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
 function holidayCountdown(holiday, days){
 returns (`It's ${days} days until ${holiday}`);
 holidayCountdown('70', easter);
+=======
+function happyHolidayTo(thirdterm, name){
+  return (`Happy ${holiday}, ${name}!`)
+}
+happyHolidayTo('easter, Armstrong');
+>>>>>>> 5fa1715935709fe3d30f3b21e621dbfc69e0aba3
